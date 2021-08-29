27e904b38cb82bbb6189873016c34da6ce8128f3
rrisskyy <riskykurniawan945@gmail.com>
2021-08-29 08:34:07 +0800

    login

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const bodyParser = require('body-parser');
 const https = require('https');
 
 const app = express();
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {
-    res.send("Hello")
+    let hook = ["Charm", "Carbon", "Captain", "Daido", "JHook", "Kaizen", "Hemus"];
+    res.render('index', {myHook: hook});
 });
+app.get("/login", (req, res) => {
+    res.render("login")
+})
 
 app.listen(3000, () => {
-    console.log("Server start at port");
+    console.log("Server start at port 3000");
 })
