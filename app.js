41036ea46f06f79e3f59fe5c426b75262665f907
rrisskyy <riskykurniawan945@gmail.com>
2021-09-18 21:44:36 +0800

    last

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const https = require('https');
 const mongoose = require('mongoose');
 const multer = require('multer');
-
+const path = require("path");
+const { addAbortSignal } = require('stream');
 
 mongoose.connect('mongodb://localhost:27017/itemsDB', {useNewUrlParser: true});
 
@@ -26,17 +27,23 @@ const itemSchema = new mongoose.Schema ({
     fileName: {
         type:String,
         required:[true, "Tolong Masukkan Tipe Barang!"]
-    },
-    fileExtension: {
-        type:String,
-        required:[true, "Tolong Masukkan Tipe Barang!"]
     }
 });
 
 const Item = mongoose.model("Item", itemSchema);
 
-const upload = multer({dest: __dirname + '/uploads/images/'});
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, __dirname + '/public/uploads/images/')
+    },
+    filename: function (req, file, cb) {
+        const extension = file.mimetype.split("/");
+      cb(null, Date.now() + "." + extension[1]) //Appending .jpg
+    }
+  })
+const upload = multer({storage: storage});
 const app = express();
+
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -49,20 +56,32 @@ app.get("/", (req, res) => {
         if (err) {
             console.log(err);
         } else {
-            let arr = [];
+            let arr = new Set();
+            let random = [];
+            let randomItems = []  
+            let recomended = []
             
+            itemsFound.forEach((item) => {
+                if (item.itemType === "Lure" || item.itemType === "Joran" || item.itemType === "Reel") {
+                    recomended.push(item);
+                }
+            })  
             const generateRandomNumber = (min, max) =>  {
                 return Math.floor(Math.random() * (max - min) + min);
             };
-                 
-            while(arr.length <= 9){
-                let r = generateRandomNumber(0, itemsFound.length);
-                if(arr.indexOf(r) === ) arr.push(itemsFound[r]);
+            while(arr.size !== 9) {
+                arr.add(generateRandomNumber(0, recomended.length));
             }
-            console.log(arr.length)
+            arr.forEach((item) => {
+                random.push(item);
+            })  
+                        
+            for (i = 0; i<9; i++) {
+                randomItems.push(recomended[random[i]]);
+            };
             res.render('index', {
                 items: itemsFound,
-                recomended: arr
+                recomended: randomItems
             });
         }
     });    
@@ -74,9 +93,54 @@ app.get("/login", (req, res) => {
 })
 
 app.get("/all", (req, res) => {
-    let allItems = ["Charm", "Carbon", "Captain", "Daido", "JHook", "Kaizen", "Hemus", "Bamboo", "Owner", "Pioneer"]
-    let allCategories = ["Joran", "Reel", "Lure", "Mata Pancing", "Nylon", "Timah Pemberat"]
-    res.render("all", {items: allItems, category:allCategories})
+    let [joran, reel, lure, hook, snap, kiliKili, nylon, timahPemberat, lainnya] = [[], [], [], [], [], [], [], [], []];
+    Item.find({}, (err, itemsFound) => {
+        if (err) {
+            console.log(err);
+        } else {
+            itemsFound.forEach((item) => {
+
+                switch(item.itemType) {
+                    case "Joran":
+                        joran.push(item);
+                        break;
+                    case "Hook":
+                        hook.push(item);
+                        break;
+                    case "Reel":
+                        reel.push(item);    
+                        break;
+                    case "Lure":
+                        lure.push(item);
+                        break;
+                    case "Snap":
+                        snap.push(item);
+                        break;
+                    case "Kili_Kili":
+                        kiliKili.push(item);
+                        break;
+                    case "Nylon":
+                        nylon.push(item);
+                        break;
+                    case "Timah_Pemberat":
+                        timahPemberat.push(item);
+                        break;
+                    default:
+                        lainnya.push(item);
+                        break;
+                };
+
+            })
+            let categories = ["Joran", "Reel", "Lure", "Mata Pancing", "Snap", "Kili Kili", "Nylon", "Timah Pemberat", "Lainnya"] 
+            let itemsCategories = [joran, reel, lure, hook, snap, kiliKili, nylon, timahPemberat, lainnya]
+            
+            res.render("all", {
+                items: itemsFound, 
+                category: categories,
+                itemsCategories: itemsCategories
+            });
+        }
+    });        
 })
 
 
@@ -100,20 +164,19 @@ app.post('/admin/post', upload.single('photo'), (req, res) => {
     const price = req.body.price;
     const itemType = req.body.itemType;
     const photoName = req.file.filename;
-    const extension = req.file.mimetype;
-
+    const extension = req.file.mimetype.split("/");
 
     const item = new Item ({
         name: itemName,
         price: price,
         itemType: itemType,
-        fileName: photoName,
-        fileExtension: extension
-
+        fileName: photoName
     });
     item.save()
 
-    res.redirect("/admin/post")
+    // setTimeout(() => { 
+    //     res.redirect("/admin/post/");
+    // }, 2000);
 
 });
 const PORT = process.env.PORT || 3000;
---
+++ public/js/script.js
@@ -0,0 +1,49 @@
+
+
+
+const btn = document.querySelector("#button");
+
+btn.addEventListener('click', () => {
+
+  const itemName = document.getElementById("itemName").value;
+  const photo = document.getElementById("formFile").value;
+  const price = document.getElementById("price").value;
+  if (itemName == "" || photo == "" || price == "") {
+  Swal.fire({
+    title: 'Gagal!',
+    text: 'Klik OK untuk melanjutkan',
+    icon: 'error',
+    confirmButtonText: 'OK'
+  })
+  } else {
+    Swal.fire({
+      title: 'Berhasil!',
+      text: 'Klik OK untuk melanjutkan',
+      icon: 'success',
+      confirmButtonText: 'OK'
+    }).then(() => { 
+      location.reload()
+  });
+    
+  }
+  
+
+})
+
+  // Swal.fire({
+  //   title: 'Apa Anda Yakin?',
+  //   text: "Jika terdapat kesalahan, anda tetap dapat mengeditnya!",
+  //   icon: 'warning',
+  //   showCancelButton: true,  
+  //   confirmButtonColor: '#3085d6',
+  //   cancelButtonColor: '#d33',
+  //   confirmButtonText: 'Ya, Posting!'
+  // }).then((result) => {
+  //   if (result.isConfirmed) {
+  //     Swal.fire(
+  //       'Berhasil!',
+  //       'Barang telah diunggah!.',
+  //       'success'
+  //     )
+  //   }
+  // })
