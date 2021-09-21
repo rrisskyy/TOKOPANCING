6fb36ce463f9cdc772f09024f6ac90e3e5393d85
rrisskyy <riskykurniawan945@gmail.com>
2021-09-21 15:37:39 +0800

    testimonial

--- public/js/script.js
+++ public/js/script.js
@@ -32,6 +32,20 @@ const swiper = new Swiper('.swiper', {
 
 });
 
+const swiper1 = new Swiper('.swiper1', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  }
+});
+
   // Swal.fire({
   //   title: 'Apa Anda Yakin?',
   //   text: "Jika terdapat kesalahan, anda tetap dapat mengeditnya!",
